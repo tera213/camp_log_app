0debd9e3be0fd61011ada30f359088c86078e762
tera213 <meganesan.t@icloud.com>
2021-03-29 19:57:18 +0900

    add new file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9aa3614493117341d720233cc275ded347268a77',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9aa3614493117341d720233cc275ded347268a77',
+    ),
+  ),
+);
